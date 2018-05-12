5aea9721bef9926dcea99ee6748b407cff5370c5
Dustin Lang <dstndstn@gmail.com>
2018-05-12 10:04:50 -0700

    select a 1b set with one exposure *not* in the set of 25

--- subs.py
+++ subs.py
@@ -25,3 +25,16 @@ for N in [1, 4, 16, 25]: #[1,5,25]:
         print('N', N, 'band', band, ': exposures', np.unique(T.expnum[np.array(Ikeep)]))
     Ikeep = np.hstack(Ikeep)
     T[Ikeep].writeto('survey-ccds-snx3-%i.fits.gz' % N)
+
+
+for N in [1, 4, 16, 25]:
+    Ikeep = []
+    for band in ['g','r','i']:
+        for chip in ['N4', 'S4']:
+            I, = np.nonzero([(c.strip() == chip) and (f.strip() == band)
+                             for c,f in zip(T.ccdname, T.filter)])
+            print(len(I), band, chip)
+            Ikeep.append(I[-N:])
+        print('N', N, 'band', band, ': exposures', np.unique(T.expnum[np.array(Ikeep)]))
+    Ikeep = np.hstack(Ikeep)
+    T[Ikeep].writeto('survey-ccds-snx3-%ib.fits.gz' % N)
